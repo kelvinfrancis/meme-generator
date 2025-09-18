00ee34fe8e5ca724d76bd05194188b6310f12e68
kelvinfrancis <kelvinmoquete0597@gmail.com>
2025-09-18 16:28:04 -0400

    feat: implement meme state management and update input handling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "Top text meme",
+    bottomText: "Bottom text meme",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleChange() {
+    console.log("Hola")
+  }
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input type="text" placeholder="One does not simply" name="topText" onChange={handleChange}/>
         </label>
 
         <label>
           Bottom Text
           <input type="text" placeholder="Walk into Mordor" name="bottomText" />
         </label>
-        
-        <button>Get a new meme image 🖼</button>
+
+        <button>Get a new meme image 🖼️</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">TOP</span>
-        <span className="bottom">BOTTOM</span>
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
