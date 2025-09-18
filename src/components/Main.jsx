005908aa910866c94f5784c79f07a9900c21cae3
kelvinfrancis <kelvinmoquete0597@gmail.com>
2025-09-18 10:52:03 -0400

    style: refactor input formatting for consistency

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,17 @@
-
 export default function Main() {
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input
-            type="text"
-            placeholder="One does not simply"
-            name="topText"
-          />
+          <input type="text" placeholder="One does not simply" name="topText" />
         </label>
 
         <label>
           Bottom Text
-          <input
-            type="text"
-            placeholder="Walk into Mordor"
-            name="bottomText"
-          />
+          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
         </label>
-        <button >Get a new meme image 🖼</button>
+        <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src="http://i.imgflip.com/1bij.jpg" />
