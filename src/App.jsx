70e14ac5edfc972dcdfea06d4d55e98b98b39978
Kelvin Moquete <kelvinmoquete0597@gmail.com>
2025-09-19 10:10:48 -0400

    feat: fetch and display Star Wars character data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [starWarsData, setStarWarsData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/11")
+    .then(res => res.json())
+    .then(data => setStarWarsData(data))
+  }, []);
+
   return (
     <>
-      <Header />
-      <Main />
+      <pre>{JSON.stringify(starWarsData)}</pre>
+      {/* <Header />
+      <Main /> */}
     </>
   );
 }
